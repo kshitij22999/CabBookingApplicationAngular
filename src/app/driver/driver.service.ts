aa4255d5e064151893a717f0b7c8016b94a5fae0
shekharkola8 <87413831+shekharkola8@users.noreply.github.com>
2021-09-06 17:14:35 +0530

    update

--- src/app/driver/driver.service.ts
+++ src/app/driver/driver.service.ts
@@ -6,12 +6,20 @@ import { Driver } from "./driver";
 @Injectable({
     providedIn: 'root'
 })
-export class DriverService{
-    baseUrl: string=' ';
+export class DriverService {
+    baseUrl: string = ' ';
 
-    constructor(private httpclient:HttpClient){}
+    constructor(private httpclient: HttpClient) { }
 
-        public addDriver(driver : Driver){
-            return this.httpclient.post<Driver>(this.baseUrl, driver);
-        }
+    public addDriver(driver: Driver) {
+        return this.httpclient.post<Driver>(this.baseUrl, driver);
     }
+
+    public deleteDriver(driver: Driver) {
+        return this.httpclient.delete<Driver>(`${this.baseUrl}/driver/${driver.id}`);
+    }
+
+    public updateDriver(driver : Driver){
+        return this.httpclient.patch<Driver>(`${this.baseUrl}/driver/${driver.id}`, driver);
+    }
+}
