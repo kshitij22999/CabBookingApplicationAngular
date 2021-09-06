85bbafb147f33e4172b44ce1606b50d48b35bdfe
kshitij22999 <kshitij22999@gmail.com>
2021-09-06 10:16:40 +0530

    updated

--- src/app/cab/cab.module.ts
+++ src/app/cab/cab.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CabListComponent } from './cab-list/cab-list.component';
+
 import { CabDetailsComponent } from './cab-details.component';
 import { CabEditComponent } from './cab-edit.component';
 import { CabAddComponent } from './cab-add.component';
+import { CabListComponent } from './cab-list.component';
 
 
 
