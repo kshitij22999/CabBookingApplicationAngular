48a6f874c1c3b1b3f77d60ab8411ce21eec728e2
shekharkola8 <87413831+shekharkola8@users.noreply.github.com>
2021-09-08 12:00:39 +0530

    update

--- src/app/driver/driver-details.component.ts
+++ src/app/driver/driver-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Driver } from './driver';
+import { DriverService } from './driver.service';
 
 @Component({
   selector: 'app-driver-details',
@@ -8,11 +10,27 @@ import { Driver } from './driver';
 })
 export class DriverDetailsComponent implements OnInit {
   
-  pageTitle = "Driver Details";
+  id: number = 0;
   driver!: Driver;
-  constructor() { }
+
+  constructor(private driverService  :DriverService,
+    private router : ActivatedRoute,
+    private route : Router) { }
 
   ngOnInit(): void {
+
+    this.driver = new Driver();
+    this.id = this.router.snapshot.params['id'];  
+
+    this.driverService.getDriverById(this.id).subscribe(data =>{
+      console.log(data)
+      this.driver=data;
+    }, error => console.log(error));
+    
   }
 
+  back(){
+    this.route.navigate(['list']);
+  }
 }
+
