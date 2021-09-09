6fb3bc8a2791137ab7fa1c7c3aa54bf0ab8396cd
shekharkola8 <87413831+shekharkola8@users.noreply.github.com>
2021-09-09 11:28:50 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { RegisterComponent } from './register/register.component';
@@ -10,10 +9,8 @@ const routes: Routes = [
   {path : 'logout' , component : LogoutComponent},
   {path : 'register' , component : RegisterComponent}
 ];
-=======
 
-const routes: Routes = [];
->>>>>>> 36a0e293d3dbcb5e7c3509b80ab35a3227ff7976
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
