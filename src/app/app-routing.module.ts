ec47bcfdda49c751fd0fbb87bd7a8a3e2611a080
kshitij22999 <kshitij22999@gmail.com>
2021-09-09 09:12:16 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CabModule } from './cab/cab.module';
 import { CustomerRoutingModule } from './customer/customer-routing.module';
+import { DriverRoutingModule } from './driver/driver-routing.module';
+import { TripBookingModule } from './TripBooking/trip-booking.module';
 
 const routes: Routes = [
-  {path:'customer',component:CustomerRoutingModule}
+  {path:'customer',component:CustomerRoutingModule},
+  {path:'cab',component:CabModule},
+  {path:'driver',component:DriverRoutingModule},
+  {path:'tripbooking',component:TripBookingModule}
 ];
 
 @NgModule({
